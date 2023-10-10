f5283e34b97be6ac377775b19e4922406ca887c4
johnny-krk <krasodomski@gmail.com>
2023-10-10 15:04:42 +0200

    Day 2 filter (a-b) isinstance(x, int), sandbox

--- sandbox.py
+++ sandbox.py
@@ -9,6 +9,7 @@ print(result)
 def square(x):
     return x ** 2
 
+
 print("power:")
 numbers = map(square, numbers)
 squared_numbers = list(numbers)
@@ -19,3 +20,14 @@ new_numbers = map(lambda x: math.sqrt(x), squared_numbers)
 print(list(new_numbers))
 
 
+# filter
+
+def is_even(item: int) -> bool:
+    return item % 2 == 0
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+filtered_results = filter(is_even, numbers)
+print(*filtered_results)
+
+result = filter(lambda x: x % 2 == 0, numbers)
+print(*result)
