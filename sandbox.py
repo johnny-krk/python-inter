b48168377a9109a242618d77bc53fb2a5c4a917c
johnny-krk <krasodomski@gmail.com>
2023-10-11 07:25:34 +0200

    Day 1 and Day 2 solutions

--- sandbox.py
+++ sandbox.py
@@ -31,3 +31,44 @@ print(*filtered_results)
 
 result = filter(lambda x: x % 2 == 0, numbers)
 print(*result)
+
+
+
+#data = [128, 121, 134, 136, 136, 118, 123, 109, 120, 116, 125, 128, 121, 129, 130, 131, 127, 119,
+#114, 134, 110, 136, 134, 125, 128, 123, 128, 133, 132, 136, 134, 129, 132]
+
+data =[18, 18, 18, 18, 19, 20, 20, 20, 21, 22, 22, 23, 24, 26, 27, 32, 33, 49, 52, 56]
+data = sorted(data)
+
+print(data)
+print(len(data))
+n = len(data) + 1
+dolny = n * 0.25
+print(dolny)
+print(data[int(dolny)], '\n')
+srodkowy = n * 0.50
+print(srodkowy)
+print(data[int(srodkowy)], '\n')
+gorny = n * 0.75
+print(gorny, int(gorny))
+print(data[int(gorny)], '\n')
+print(27+ 5* 0.75)
+
+
+import statistics
+
+values = [23, 26, 29, 30, 32, 34, 37, 45, 57, 80, 102, 147, 210, 355, 782, 1209]
+values_median = statistics.median(values)
+print(values_median)
+
+values_median_l = statistics.median_low(values)
+print(values_median_l)
+
+
+import numpy
+
+print("0.25: ", numpy.quantile(values, 0.25))
+print("0.5: ", numpy.quantile(values, 0.5))
+print("0.75: ", numpy.quantile(values, 0.75))
+values_median_h = statistics.median_high(values)
+print(values_median_h)
